518fd1b7e9c4fc1bc056df2dac91dfa0907cb12a
vitorbizarra <vitorbizarracontato@gmail.com>
2024-03-23 17:20:44 -0300

    :package: build: @lottiefiles/dotlottie-web

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs'
+import { DotLottie } from '@lottiefiles/dotlottie-web';
 
 window.Alpine = Alpine
+
+window.DotLottie = DotLottie
 
 Alpine.start()
