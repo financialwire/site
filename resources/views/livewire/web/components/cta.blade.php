90f333d7cdb8bc6ebd69c613287791cd04af2a18
vitorbizarra <vitorbizarracontato@gmail.com>
2024-03-23 23:48:49 -0300

    :sparkles: feat: CTA

---
+++ resources/views/livewire/web/components/cta.blade.php
@@ -0,0 +1,22 @@
+<div class="bg-financialwire-purple">
+    <div class="max-w-screen-xl mx-auto max-xl:px-4">
+        <div class="grid grid-cols-1 xl:grid-cols-2 gap-8 max-xl:py-4 items-center">
+            <img src="{{ asset('images/mockup.png') }}" alt="Mockup" class="w-full max-xl:max-w-screen-sm max-xl:mx-auto xl:scale-105">
+
+            <div class="flex flex-col max-xl:mx-auto max-xl:max-w-screen-sm gap-6 text-white max-xl:text-center">
+                <h2 class="font-bold text-3xl md:text-4xl lg:text-5xl">
+                    Tenha acesso à plataforma em qualquer dispositivo
+                </h2>
+                <p class="text-lg lg:text-xl">
+                    Seja no computador, celular ou tablet com conexão a internet, você pode acessar a nossa plataforma
+                    quando e onde quiser.
+                </p>
+
+                <a href="#"
+                    class="w-fit px-6 lg:px-0 lg:w-full max-xl:mx-auto rounded-lg bg-black text-sm lg:text-lg text-white text-center font-inter font-bold py-3 lg:py-6 max-w-96 uppercase hover:bg-white hover:text-black transition-all ease-in-out duration-300">
+                    Comece agora
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/web/home.blade.php
+++ resources/views/livewire/web/home.blade.php
@@ -1,5 +1,7 @@
-<div class="max-w-screen-xl mx-auto max-xl:px-4 space-y-16">
+<div class="space-y-16">
     <livewire:web.components.hero />
 
     <livewire:web.components.benefits />
+
+    <livewire:web.components.cta />
 </div>
