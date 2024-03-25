7f3cdbe829239d8c050c88aa37949c8c2acd023d
vitorbizarra <vitorbizarracontato@gmail.com>
2024-03-25 20:01:06 -0300

    :lipstick: feat: Hero section

--- resources/views/livewire/web/components/hero.blade.php
+++ resources/views/livewire/web/components/hero.blade.php
@@ -2,17 +2,17 @@
 
     <div class="h-full grid grid-cols-1 lg:grid-cols-2 items-center">
         <div class="flex flex-col gap-4 sm:gap-6 max-lg:max-w-screen-sm max-lg:mx-auto max-lg:mb-auto">
-            <h1 class="font-bold text-4xl lg:text-6xl max-lg:text-center">
+            <h1 class="font-bold text-4xl lg:text-6xl">
                 Sua plataforma de controle financeiro
             </h1>
-            <p class="text-xl lg:text-2xl max-lg:text-center">
+            <p class="text-xl lg:text-2xl">
                 Gerencie suas contas, transações e muito mais com uma plataforma intuitiva e sem complicações.
                 <br><br class="max-lg:hidden">
                 Seu dinheiro, suas regras, sua liberdade.
             </p>
 
             <a href="{{ env('DASH_REGISTRATION_URL') }}"
-                class="w-fit px-6 lg:px-0 lg:w-full max-lg:mx-auto rounded-lg bg-financialwire-purple text-sm lg:text-lg text-white text-center font-inter font-bold py-3 lg:py-6 max-w-96 uppercase hover:bg-black transition-all ease-in-out duration-300">
+                class="px-6 lg:px-0 w-full max-lg:mx-auto rounded-lg bg-financialwire-purple text-lg text-white text-center font-inter font-bold py-6 lg:py-6 max-w-96 uppercase hover:bg-black transition-all ease-in-out duration-300">
                 Comece agora
             </a>
         </div>
