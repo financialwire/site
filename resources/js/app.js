8a8876bc2be02b15de1682200feaa02eba2e07b2
vitorbizarra <vitorbizarracontato@gmail.com>
2024-04-26 19:30:11 -0300

    :fire: Removendo Alpine.js como dependência node

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,6 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs'
 import { DotLottie } from '@lottiefiles/dotlottie-web';
-
-window.Alpine = Alpine
 
 window.DotLottie = DotLottie
 
-Alpine.start()
