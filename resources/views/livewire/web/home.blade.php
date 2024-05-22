c04dceea0fd76f0dd382ecddb24fc10677f608b2
vitorbizarra <vitorbizarracontato@gmail.com>
2024-05-21 22:16:37 -0300

    :construction: personalization.blade.php

--- resources/views/livewire/web/home.blade.php
+++ resources/views/livewire/web/home.blade.php
@@ -1,9 +1,11 @@
-<div>
+<div class="space-y-16 lg:space-y-32">
     <livewire:web.components.hero />
 
     <livewire:web.components.benefits />
 
     <livewire:web.components.control />
+
+    <livewire:web.components.personalization />
 
     <livewire:web.components.cta />
 
